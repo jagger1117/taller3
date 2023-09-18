bac3b0e2a15d7aedd1a484fcd652298b5fc23dd4
jagger1117 <143247664+jagger1117@users.noreply.github.com>
2023-09-18 14:38:44 -0500

    Add files via upload

---
+++ -ejercicio23/src/ejercicio23/Ejercicio23.java
@@ -0,0 +1,9 @@
+package ejercicio23;
+public class Ejercicio23 {
+    public static void main(String[] args) {
+          formulario form=new formulario();
+        form.setVisible(true);
+
+    }
+    
+}
---
+++ -ejerciciogeometria/src/ejerciciogeometria/Ejerciciogeometria.java
@@ -0,0 +1,12 @@
+
+package ejerciciogeometria;
+
+public class Ejerciciogeometria {
+
+    public static void main(String[] args) {
+        formulario form=new formulario();
+        form.setVisible(true);
+
+    }
+    
+}
